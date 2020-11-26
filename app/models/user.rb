42eececa0f5a0c09c436a24802167ca01b03fbd4
kiserukun <73691576+kiserukun@users.noreply.github.com>
2020-11-26 16:42:48 +0900

    with_options

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/ }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ンー]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン一]+\z/ }
-  validates :birthday, presence: true
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  with_options presence: true do
+   validates :name
+   validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]/ }
+   validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]/ }
+   validates :first_name_kana, format: { with: /\A[ァ-ンー]+\z/ }
+   validates :last_name_kana, format: { with: /\A[ァ-ン一]+\z/ }
+   validates :birthday
+   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  end
 end
