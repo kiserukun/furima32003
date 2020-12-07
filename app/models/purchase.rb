5f996cecf590956021b1071654986c451132d4f2
kiserukun <73691576+kiserukun@users.noreply.github.com>
2020-12-07 14:24:46 +0900

    日本語エラー

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,7 +4,7 @@ class Purchase
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
-    validates :genre_prefecture_id, numericality: { other_than: 1 }
+    validates :genre_prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
     validates :city
     validates :house_number
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
